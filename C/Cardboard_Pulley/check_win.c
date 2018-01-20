2173a7cc835f4ad0b9867be283e792f5b23bec22
Bouftoudesuite <habi_a@etna-alternance.net>
2018-01-20 23:01:57 +0100

    C

--- C/Cardboard_Pulley/check_win.c
+++
@@ -1,8 +0,0 @@
-#include "struct.h"
-
-int check_win(t_char *player)
-{
-    if (player->coord[E_X] < 0 || player->coord[E_Y] < 0)
-        return 1;
-    return 0;
-}
--- C/Cardboard_Pulley/clear_screen.c
+++
@@ -1,6 +0,0 @@
-#include "struct.h"
-
-void clear_screen()
-{
-    my_putstr("\033[1;1H\033[2J");
-}
