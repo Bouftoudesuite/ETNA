b2047d7e5ce3436d8165c5435c4f1c2862cb6be1
Habi Açal <habi_a@etna-alternance.net>
2018-01-31 22:06:08 +0100

    C

---
+++ C++/Piscine/Jour01/Etape02/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int		main(int argc, char *argv[])
+{
+  int		i;
+  std::string	str;
+
+  std::cin >> str;
+  while (str != "quit")
+  {
+    i = 0;
+    while (i < str.size())
+    {
+      if (str[i] >= 'a' && str[i] <= 'z')
+	str[i] -= 32;
+      i++;
+    }
+    std::cout << str << std::endl;
+    std::cin >> str;
+  }
+  return (0);
+}
