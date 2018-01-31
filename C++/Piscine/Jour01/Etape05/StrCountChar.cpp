a249191ad51ca8c709b4a103c832c40afe5ef432
Habi Açal <habi_a@etna-alternance.net>
2018-01-31 23:06:30 +0100

    C++

---
+++ C++/Piscine/Jour01/Etape05/StrCountChar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int		StrCountChar(std::string str, char c)
+{
+  int		count;
+  unsigned int	i;
+
+  i = 0;
+  count = 0;
+  while (i < str.size())
+  {
+    if (str[i] == c)
+      count++;
+    i++;
+  }
+  return count;
+}
