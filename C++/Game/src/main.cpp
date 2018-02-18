0b2d0ceebb38384e16220d695f07856a773e31e2
Habi Açal <habi_a@etna-alternance.net>
2018-02-19 00:28:19 +0100

    IN DEV..

--- C++/Game/src/main.cpp
+++ C++/Game/src/main.cpp
@@ -24,7 +24,6 @@ int main()
                             menu.MoveDown();
                             break;
                     }
-                    break;
                 case sf::Event::Closed:
                     window.close();
                     break;
