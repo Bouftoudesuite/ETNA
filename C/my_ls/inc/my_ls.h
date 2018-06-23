3ea9919a6d303352b899e442b5c2183324243355
Açal Habi <habi_a@etna-alternance.net>
2018-06-23 20:55:17 +0200

    src inc

---
+++ C/my_ls/inc/my_ls.h
@@ -0,0 +1,7 @@
+#ifndef _MY_LS_MY_LS_H_
+#define _MY_LS_MY_LS_H_
+
+int my_ls(const char *path);
+int my_ls_l(const char *path);
+
+#endif /*_MY_LS_MY_LS_H_*/
---
+++ C/my_ls/src/my_ls_l.c
@@ -0,0 +1,30 @@
+/*
+** my_ls.c for my_ls in /Users/habi_a/Documents/ETNA/C/my_ls
+**
+** Made by HABI Açal
+** Login   <habi_a@etna-alternance.net>
+**
+** Started on  Mon Mar 19 11:24:00 2018 HABI Açal
+** Last update Mon Mar 19 11:42:01 2018 HABI Açal
+*/
+#include <dirent.h>
+#include <my.h>
+#include <stdlib.h>
+
+int						my_ls_l(const char *path)
+{
+    struct dirent		*d;
+    DIR					*dir;
+
+    dir = opendir(path);
+    if (dir == NULL)
+        return (0);
+    while (( d = readdir(dir)))
+    {
+        my_putstr(d->d_name);
+        my_putchar('\t');
+    }
+    closedir(dir);
+    my_putchar('\n');
+    return (0);
+}
